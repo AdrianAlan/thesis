bee78bc22dbd7dce6056513ab351710fa5b9f86b
AdrianAlan <adrianalan.pol@gmail.com>
2015-09-25 09:33:19 -0700

    adding remote controller

--- topologies/simple.py
+++ topologies/simple.py
@@ -1,32 +1,34 @@
 #!/usr/bin/python
 
-#from mininet.net import Mininet
+from mininet.net import Mininet
 #from mininet.node import OVSSwitch
 from mininet.topo import Topo
+from mininet.node import RemoteController
 from mininet.log import setLogLevel
-#from mininet.cli import CLI
+from mininet.cli import CLI
 
 setLogLevel('info')
 
-class Simplest(Topo):
+def empty():
+    net = Mininet(controller=RemoteController)
+    net.addController('c0', controller=RemoteController, ip='10.0.3.11', port=6633)
 
-    def __init__(self, **opts):
-        Topo.__init__(self, **opts)
-        
-        switches = []
-        for i in range(4):
-            switches.append(self.addSwitch('s'+str(i + 1), protocols="OpenFlow13"))
-        hosts = []
-        for i in range(2):
-            hosts.append(self.addHost('h'+str(i+1)))
+    switches = []
+    for i in range(4):
+        switches.append(net.addSwitch('s'+str(i + 1), protocols="OpenFlow13"))
+    hosts = []
+    for i in range(2):
+        hosts.append(net.addHost('h'+str(i+1)))
 
-        self.addLink(switches[0], hosts[0])
-        self.addLink(switches[3], hosts[1])
-        self.addLink(switches[0], switches[1])
-        self.addLink(switches[0], switches[2])
-        self.addLink(switches[3], switches[1])
-        self.addLink(switches[3], switches[2])
+    net.addLink(switches[0], hosts[0])
+    net.addLink(switches[3], hosts[1])
+    net.addLink(switches[0], switches[1])
+    net.addLink(switches[0], switches[2])
+    net.addLink(switches[3], switches[1])
+    net.addLink(switches[3], switches[2])
 
-topos = {
-    'T0': Simplest
-}
+    net.start()
+    CLI(net)
+    net.stop()
+
+empty()
