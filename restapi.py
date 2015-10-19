d1a6f10da02e7ec282bc01256503389fdde5162a
Basic ONOS Tutorial <adrianalan.pol@gmail.com>
2015-10-19 14:54:48 -0700

    starting stoping and cleaning mininet via rest

--- restapi.py
+++ restapi.py
@@ -1,9 +1,38 @@
 """REST API run script"""
 from flask import Flask
-#from subprocess import call
+from mininet.net import Mininet
+from mininet.node import RemoteController
+from subprocess import call
+from topologies import simple
+from topologies import kpnnl
+from topologies import sprint
 
 app = Flask(__name__)
 VERSION = "0.0"
+NET = Mininet(controller=RemoteController)
+
+@app.route('/clean')
+def clean():
+    """Cleans the mess"""
+    call(['sudo', 'mn', '-c'])
+    return "Success"
+
+@app.route('/start/<topo>')
+def start(topo):
+    """Starts network"""
+    if topo == "simple":
+        simple.empty(NET)
+    elif topo == "kpnnl":
+        kpnnl.kpn(NET)
+    elif topo == "sprint":
+        sprint.sprint(NET)
+    return "Success"
+
+@app.route('/stop')
+def stop():
+    """Stops network and cleans"""
+    NET.stop()
+    return "Success"
 
 @app.route('/version')
 def version():
