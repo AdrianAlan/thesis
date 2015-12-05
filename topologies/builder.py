5be3cd92b08bd3397f9ab4af5f26b28590dec972
AdrianAlan <adrianalan.pol@gmail.com>
2015-12-05 05:43:13 -0800

    changed mininet configuration

--- topologies/builder.py
+++ topologies/builder.py
@@ -1,122 +1,125 @@
 #!/usr/bin/python
 
-from mininet.net import Mininet
 from mininet.topo import Topo
 from mininet.node import RemoteController
 from mininet.log import setLogLevel
-from mininet.cli import CLI
 
 setLogLevel('info')
-FIX_TIME = 1
 
 def add_controller(net):
-    net.addController('c0', controller=RemoteController, ip='10.0.3.11', port=6633)
+    """Adds controller, listen on port 6633"""
+    net.addController('c0', controller=RemoteController, ip='10.0.2.15', port=6633)
 
-def simple(net):
+def create_simple(net):
+    """Simple topology with 4 switches and 2 hosts"""
+
+    # add four OpenFlow 1.3 nodes
     switches = []
     for i in range(4):
         switches.append(net.addSwitch('s'+str(i + 1), protocols="OpenFlow13"))
+
+    # add two hosts
     hosts = []
     for i in range(2):
-        hosts.append(net.addHost('h'+str(i+1)))
+        hosts.append(net.addHost('h'+str(i + 1)))
 
-    net.addLink(switches[0], hosts[0])
-    net.addLink(switches[3], hosts[1])
-    net.addLink(switches[0], switches[1])
-    net.addLink(switches[0], switches[2])
-    net.addLink(switches[3], switches[1])
-    net.addLink(switches[3], switches[2])
+    # create topology
+    net.addLink(switches[0], hosts[0], bw=1, max_queue_size=1)
+    net.addLink(switches[3], hosts[1], bw=1, max_queue_size=1)
+    net.addLink(switches[0], switches[1], bw=1, max_queue_size=1)
+    net.addLink(switches[0], switches[2], bw=1, max_queue_size=1)
+    net.addLink(switches[3], switches[1], bw=1, max_queue_size=1)
+    net.addLink(switches[3], switches[2], bw=1, max_queue_size=1)
+
+    # start network
     net.start()
 
-def kpnnl(net):
+def create_kpnnl(net):
+    """KPN Dutch Network, bandwidth and queue size are arbitrary"""
+
     switches = {}
     hosts = {}
+    j = 1
     for i, p in enumerate(['Alkmaar', 'Amsterdam', 'Arnhem', 'Breda',
                            'DenBosch', 'DenHaag', 'Eindhoven', 'Enschede',
                            'Groningen', 'Heerlen', 'Hengelo', 'Leenwarden',
                            'Maastricht', 'Rotterdam', 'Roermond', 'Utrecht',
                            'Venlo', 'Zwolle']):
-        switches[p] = net.addSwitch('s' + str(i), protocols="OpenFlow13")
-        hosts[p] = net.addHost('h' + str(i))
+        switches[p] = net.addSwitch('s' + str(i + 1), protocols="OpenFlow13")
 
-    for i in ['Alkmaar', 'Amsterdam', 'Arnhem', 'Breda', 'DenBosch', 'DenHaag',
-              'Eindhoven', 'Enschede', 'Groningen', 'Heerlen', 'Hengelo',
-              'Leenwarden', 'Maastricht', 'Rotterdam', 'Roermond', 'Utrecht',
-              'Venlo', 'Zwolle']:
-        net.addLink(switches[i], hosts[i])
+        # add hosts that will generate traffic in simulations
+        if p in ['Amsterdam', 'Maastricht']:
+            host = net.addHost('h' + str(j))
+            j += 1
+            net.addLink(switches[p], host)
 
     # add connections between switches
-    net.addLink(switches['Alkmaar'], switches['Amsterdam'])
-    net.addLink(switches['Alkmaar'], switches['DenHaag'])
-    net.addLink(switches['Alkmaar'], switches['Leenwarden'])
-    net.addLink(switches['Amsterdam'], switches['DenHaag'])
-    net.addLink(switches['Amsterdam'], switches['Utrecht'])
-    net.addLink(switches['Amsterdam'], switches['Zwolle'])
-    net.addLink(switches['Arnhem'], switches['DenBosch'])
-    net.addLink(switches['Arnhem'], switches['Enschede'])
-    net.addLink(switches['Arnhem'], switches['Hengelo'])
-    net.addLink(switches['Arnhem'], switches['Utrecht'])
-    net.addLink(switches['Arnhem'], switches['Venlo'])
-    net.addLink(switches['Arnhem'], switches['Zwolle'])
-    net.addLink(switches['Breda'], switches['DenBosch'])
-    net.addLink(switches['Breda'], switches['Eindhoven'])
-    net.addLink(switches['Breda'], switches['Rotterdam'])
-    net.addLink(switches['Breda'], switches['Utrecht'])
-    net.addLink(switches['DenBosch'], switches['Eindhoven'])
-    net.addLink(switches['DenBosch'], switches['Utrecht'])
-    net.addLink(switches['DenBosch'], switches['Venlo'])
-    net.addLink(switches['DenHaag'], switches['Rotterdam'])
-    net.addLink(switches['DenHaag'], switches['Utrecht'])
-    net.addLink(switches['Eindhoven'], switches['Roermond'])
-    net.addLink(switches['Eindhoven'], switches['Venlo'])
-    net.addLink(switches['Enschede'], switches['Hengelo'])
-    net.addLink(switches['Groningen'], switches['Hengelo'])
-    net.addLink(switches['Groningen'], switches['Leenwarden'])
-    net.addLink(switches['Groningen'], switches['Zwolle'])
-    net.addLink(switches['Heerlen'], switches['Roermond'])
-    net.addLink(switches['Heerlen'], switches['Maastricht'])
-    net.addLink(switches['Hengelo'], switches['Zwolle'])
-    net.addLink(switches['Leenwarden'], switches['Zwolle'])
-    net.addLink(switches['Maastricht'], switches['Roermond'])
-    net.addLink(switches['Rotterdam'], switches['Utrecht'])
-    net.addLink(switches['Roermond'], switches['Venlo'])
-    net.addLink(switches['Utrecht'], switches['Zwolle'])
+    net.addLink(switches['Alkmaar'], switches['Amsterdam'], bw=1, max_queue_size=1)
+    net.addLink(switches['Alkmaar'], switches['DenHaag'], bw=1, max_queue_size=1)
+    net.addLink(switches['Alkmaar'], switches['Leenwarden'], bw=1, max_queue_size=1)
+    net.addLink(switches['Amsterdam'], switches['DenHaag'], bw=1, max_queue_size=1)
+    net.addLink(switches['Amsterdam'], switches['Utrecht'], bw=1, max_queue_size=1)
+    net.addLink(switches['Amsterdam'], switches['Zwolle'], bw=1, max_queue_size=1)
+    net.addLink(switches['Arnhem'], switches['DenBosch'], bw=1, max_queue_size=1)
+    net.addLink(switches['Arnhem'], switches['Enschede'], bw=1, max_queue_size=1)
+    net.addLink(switches['Arnhem'], switches['Hengelo'], bw=1, max_queue_size=1)
+    net.addLink(switches['Arnhem'], switches['Utrecht'], bw=1, max_queue_size=1)
+    net.addLink(switches['Arnhem'], switches['Venlo'], bw=1, max_queue_size=1)
+    net.addLink(switches['Arnhem'], switches['Zwolle'], bw=1, max_queue_size=1)
+    net.addLink(switches['Breda'], switches['DenBosch'], bw=1, max_queue_size=1)
+    net.addLink(switches['Breda'], switches['Eindhoven'], bw=1, max_queue_size=1)
+    net.addLink(switches['Breda'], switches['Rotterdam'], bw=1, max_queue_size=1)
+    net.addLink(switches['Breda'], switches['Utrecht'], bw=1, max_queue_size=1)
+    net.addLink(switches['DenBosch'], switches['Eindhoven'], bw=1, max_queue_size=1)
+    net.addLink(switches['DenBosch'], switches['Utrecht'], bw=1, max_queue_size=1)
+    net.addLink(switches['DenBosch'], switches['Venlo'], bw=1, max_queue_size=1)
+    net.addLink(switches['DenHaag'], switches['Rotterdam'], bw=1, max_queue_size=1)
+    net.addLink(switches['DenHaag'], switches['Utrecht'], bw=1, max_queue_size=1)
+    net.addLink(switches['Eindhoven'], switches['Roermond'], bw=1, max_queue_size=1)
+    net.addLink(switches['Eindhoven'], switches['Venlo'], bw=1, max_queue_size=1)
+    net.addLink(switches['Enschede'], switches['Hengelo'], bw=1, max_queue_size=1)
+    net.addLink(switches['Groningen'], switches['Hengelo'], bw=1, max_queue_size=1)
+    net.addLink(switches['Groningen'], switches['Leenwarden'], bw=1, max_queue_size=1)
+    net.addLink(switches['Groningen'], switches['Zwolle'], bw=1, max_queue_size=1)
+    net.addLink(switches['Heerlen'], switches['Roermond'], bw=1, max_queue_size=1)
+    net.addLink(switches['Heerlen'], switches['Maastricht'], bw=1, max_queue_size=1)
+    net.addLink(switches['Hengelo'], switches['Zwolle'], bw=1, max_queue_size=1)
+    net.addLink(switches['Leenwarden'], switches['Zwolle'], bw=1, max_queue_size=1)
+    net.addLink(switches['Maastricht'], switches['Roermond'], bw=1, max_queue_size=1)
+    net.addLink(switches['Rotterdam'], switches['Utrecht'], bw=1, max_queue_size=1)
+    net.addLink(switches['Roermond'], switches['Venlo'], bw=1, max_queue_size=1)
+    net.addLink(switches['Utrecht'], switches['Zwolle'], bw=1, max_queue_size=1)
     net.start()
 
-def sprint(net):
+def create_sprint(net):
+    """US Sprint network, bandwidth and queue size are arbitrary"""
+
     switches = {}
     hosts = {}
     for i, p in enumerate(['Anaheim', 'Atalanta', 'Boulder', 'Cheyenne',
                            'Chicago', 'Fort Worth', 'Kansas City', 'New York',
                            'Seattle', 'Stockton', 'Washington']):
-        switches[p] = net.addSwitch('s' + str(i), protocols="OpenFlow13")
-        hosts[p] = net.addHost('h' + str(i))
-
-    for i in ['Anaheim', 'Atalanta', 'Boulder', 'Cheyenne', 'Chicago',
-              'Fort Worth', 'Kansas City', 'New York', 'Seattle', 'Stockton',
-              'Washington']:
-        net.addLink(switches[i], hosts[i])
+        switches[p] = net.addSwitch('s' + str(i + 1), protocols="OpenFlow13")
+        host = net.addHost('h' + str(i + 1))
+        net.addLink(switches[p], host)
 
     # add connections between switches
-    net.addLink(switches['Anaheim'], switches['Fort Worth'])
-    net.addLink(switches['Anaheim'], switches['Stockton'])
-    net.addLink(switches['Atalanta'], switches['Fort Worth'])
-    net.addLink(switches['Atalanta'], switches['Washington'])
-    net.addLink(switches['Boulder'], switches['Cheyenne'])
-    net.addLink(switches['Cheyenne'], switches['Kansas City'])
-    net.addLink(switches['Cheyenne'], switches['Stockton'])
-    net.addLink(switches['Chicago'], switches['New York'])
-    net.addLink(switches['Chicago'], switches['Seattle'])
-    net.addLink(switches['Chicago'], switches['Stockton'])
-    net.addLink(switches['Fort Worth'], switches['Kansas City'])
-    net.addLink(switches['Fort Worth'], switches['Washington'])
-    net.addLink(switches['Kansas City'], switches['Washington'])
-    net.addLink(switches['New York'], switches['Stockton'])
-    net.addLink(switches['New York'], switches['Washington'])
-    net.addLink(switches['Seattle'], switches['Stockton'])
-    net.addLink(switches['Washington'], switches['Stockton'])
+    net.addLink(switches['Anaheim'], switches['Fort Worth'], bw=1, max_queue_size=10)
+    net.addLink(switches['Anaheim'], switches['Stockton'], bw=1, max_queue_size=10)
+    net.addLink(switches['Atalanta'], switches['Fort Worth'], bw=1, max_queue_size=10)
+    net.addLink(switches['Atalanta'], switches['Washington'], bw=1, max_queue_size=10)
+    net.addLink(switches['Boulder'], switches['Cheyenne'], bw=1, max_queue_size=10)
+    net.addLink(switches['Cheyenne'], switches['Kansas City'], bw=1, max_queue_size=10)
+    net.addLink(switches['Cheyenne'], switches['Stockton'], bw=1, max_queue_size=10)
+    net.addLink(switches['Chicago'], switches['New York'], bw=1, max_queue_size=10)
+    net.addLink(switches['Chicago'], switches['Seattle'], bw=1, max_queue_size=10)
+    net.addLink(switches['Chicago'], switches['Stockton'], bw=1, max_queue_size=10)
+    net.addLink(switches['Fort Worth'], switches['Kansas City'], bw=1, max_queue_size=10)
+    net.addLink(switches['Fort Worth'], switches['Washington'], bw=1, max_queue_size=10)
+    net.addLink(switches['Kansas City'], switches['Washington'], bw=1, max_queue_size=10)
+    net.addLink(switches['New York'], switches['Stockton'], bw=1, max_queue_size=10)
+    net.addLink(switches['New York'], switches['Washington'], bw=1, max_queue_size=10)
+    net.addLink(switches['Seattle'], switches['Stockton'], bw=1, max_queue_size=10)
+    net.addLink(switches['Washington'], switches['Stockton'], bw=1, max_queue_size=10)
 
     net.start()
-
-def ping(net, h1, h2, i):
-    return net.custom_ping([net.getNodeByName(h1), net.getNodeByName(h2)], interval=i, count=FIX_TIME/i, timeout=2)
